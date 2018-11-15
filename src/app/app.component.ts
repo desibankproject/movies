4c05cff3bab36cbf1f1e0d734d842eda658c5703
Synergistisitit <synergisticit2020@gmail.com>
2018-11-15 15:34:14 -0800

    Create New Component  + Event Binding + Factorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Fact } from './model/fact.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public name = 'movies';
-  public email = "nagen@synergisticit";
+  private name = 'movies';
+  private email = "Welcome to Mathematics!!!!!!!!!!";
+  private pnum:number=3;
+  private result:number=0;
+  private results:Fact[]=[];
+  private sno:number=1;
+
+  public deleteFact(data):void{
+     this.results=this.results.filter(t => t!=data);
+    console.log(data);
+  }
+
+  /**
+   * This is method definition for TypeScript
+   */
+  public calFact() : void {
+    console.log("Fact is called!!!!!!!!!!");
+    let fact=1;
+    for(let x=2;x<=this.pnum;x++){
+       fact=x*fact;
+    }
+
+    this.result=fact;
+    let row=new Fact(this.sno,this.pnum,fact);
+    this.results.push(row);
+    this.sno=this.sno+1;
+    
+  } 
+
 }
 
 
